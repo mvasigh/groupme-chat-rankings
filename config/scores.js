d8d8a1540d5d4ae733190580e39022b67c14eec7
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-05-25 16:01:49 -0500

    modify score values for triggers

--- config/scores.js
+++ config/scores.js
@@ -1,14 +1,14 @@
 module.exports = {
-  like: 10,
+  like: 40,
   triggers: {
-    lol: 10,
-    LOL: 10,
-    lmao: 25,
-    LMAO: 50,
-    'lmao!': 50,
-    'LMAO!': 100,
-    lmfao: 100,
-    LMFAO: 300,
-    'LMFAO!': 1000
+    lol: 30,
+    LOL: 40,
+    lmao: 100,
+    LMAO: 200,
+    'lmao!': 200,
+    'LMAO!': 400,
+    lmfao: 400,
+    LMFAO: 800,
+    'LMFAO!': 1150
   }
 };
