3df74c02cdaff55b3d63df9df2aa5ac690df2955
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-05-25 16:02:23 -0500

    create working GroupChat module

---
+++ js/GroupChat.js
@@ -0,0 +1,64 @@
+const AXIOS_CONFIG = require('../config/axios');
+const axios = require('axios').create(AXIOS_CONFIG);
+
+function getLeaderboard(groupId, period = 'week') {
+  const periodOpts = ['day', 'week', 'month'];
+  if (!periodOpts.includes(period)) period = 'week';
+
+  return axios
+    .get(`/groups/${groupId}/likes?period=${period}`)
+    .then(response => response.data.response.messages)
+    .then(messages => messages.filter(msg => msg.name != 'GroupMe'))
+    .catch(err => console.log(err));
+}
+
+function getMessageReplies(messageId, groupId) {
+  return axios
+    .get(`/groups/${groupId}/messages?after_id=${messageId}&limit=10`)
+    .then(response => response.data.response.messages)
+    .catch(err => console.log(err));
+}
+
+function getMessageScore(message, scoreConfig) {
+  let score = scoreConfig.like * message.favorited_by.length;
+  const triggers = Object.keys(scoreConfig.triggers);
+  // TODO: Use RegExp for this. Maybe change the triggers config to regex strings
+  // and create a new regexp object for each trigger here
+
+  // Use String.prototype.match() to match against regex.
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
+  return getMessageReplies(message.id, message.group_id)
+    .then(replies => {
+      replies.forEach(reply => {
+        triggers.forEach(trigger => {
+          if (reply.text && reply.text.includes(trigger)) {
+            score += scoreConfig.triggers[trigger];
+          }
+        });
+      });
+      return score;
+    })
+    .catch(e => console.log(e));
+}
+
+function getRankings(groupId, scoreConfig, period = 'week') {
+  return getLeaderboard(groupId, 'week').then(entries => {
+    return entries.map(entry => {
+      return getMessageScore(entry, scoreConfig).then(score => {
+        return {
+          name: entry.name,
+          text: entry.text,
+          attachments: entry.attachments,
+          score
+        };
+      });
+    });
+  });
+}
+
+module.exports = {
+  getLeaderboard,
+  getMessageReplies,
+  getMessageScore,
+  getRankings
+};
