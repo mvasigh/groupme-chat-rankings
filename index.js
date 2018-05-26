be320c1d1f6c423be1558783b793abc97dc2e53e
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-05-26 03:26:02 -0500

    update static files directory and add catch-all route

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express'),
+  path = require('path'),
   rankingsRoutes = require('./routes/api/rankings');
 
 const app = express();
 
 // APP CONFIG
 // ==========
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'client/public'));
 app.set('view engine', 'ejs');
 
 // ROUTES CONFIG
 // =============
 app.use('/api/rankings', rankingsRoutes);
+app.get('/', (req, res) => res.redirect('/api/rankings'));
+app.get('*', (req, res) => {
+  res.send('Page not found.');
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
