091831876dcb207f015dc8f9cc33bb8721403910
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-09 23:44:26 -0500

    remake SCORE_CONFIG using regex

--- config/scores.js
+++ config/scores.js
@@ -1,14 +1,55 @@
-module.exports = {
-  like: 70,
-  triggers: {
-    lol: 70,
-    LOL: 90,
-    lmao: 210,
-    LMAO: 330,
-    'lmao!': 330,
-    'LMAO!': 400,
-    lmfao: 400,
-    LMFAO: 800,
-    'LMFAO!': 1150
-  }
+const SCORE_CONFIG = {
+  base: {
+    like: 70,
+    multiplier: 1.2
+  },
+  triggers: [
+    {
+      name: 'lol',
+      pattern: /lo+l/,
+      score: 35
+    },
+    {
+      name: 'LOL',
+      pattern: /LO+L/,
+      score: 60
+    },
+    {
+      name: 'lmao',
+      pattern: /lmao+/,
+      score: 150
+    },
+    {
+      name: 'LMAO',
+      pattern: /LMAO+/,
+      score: 210
+    },
+    {
+      name: 'lmao!',
+      pattern: /lmao+!+/,
+      score: 330
+    },
+    {
+      name: 'LMAO!',
+      pattern: /LMAO+!+/,
+      score: 440
+    },
+    {
+      name: 'lmfao',
+      pattern: /lmfao+/,
+      score: 350
+    },
+    {
+      name: 'LMFAO',
+      pattern: /LMFAO+/,
+      score: 480
+    },
+    {
+      name: 'LMFAO!',
+      pattern: /LMFAO+!+/,
+      score: 800
+    }
+  ]
 };
+
+module.exports = SCORE_CONFIG;
