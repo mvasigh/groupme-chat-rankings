3255a5a2dc673c8024f73d348531af8a9f7cae97
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-26 01:09:04 -0400

    scaffold MessageList component

---
+++ client/src/components/MessageList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MessageList extends Component {
+  renderMessages() {
+    return;
+  }
+
+  render() {
+    return <ol>{this.renderMessages()}</ol>;
+  }
+}
+
+export default MessageList;
