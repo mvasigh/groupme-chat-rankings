c785eb12ff85a129aec142b401b2618c3a3af697
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-26 01:08:18 -0400

    create action types

---
+++ client/src/actions/types.js
@@ -0,0 +1,4 @@
+export const GET_DAY_RANKINGS = 'get_day_rankings';
+export const GET_WEEK_RANKINGS = 'get_week_rankings';
+export const GET_MONTH_RANKINGS = 'get_month_rankings';
+export const FILTER_PERIOD = 'filter_period';
