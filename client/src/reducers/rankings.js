779954aa4851f8ad4cc1632a8eb528e121697bc9
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-26 13:03:34 -0400

    add various 'getRankings' action types to rankings reducer

--- client/src/reducers/rankings.js
+++ client/src/reducers/rankings.js
@@ -1,11 +1,17 @@
-const initialState = {
-  day: [],
-  week: [],
-  month: []
-};
+import {
+  GET_DAY_RANKINGS,
+  GET_WEEK_RANKINGS,
+  GET_MONTH_RANKINGS
+} from '../actions/types';
 
-export default function rankingsReducer(state = initialState, action) {
+export default function rankingsReducer(state = {}, action) {
   switch (action.type) {
+    case GET_DAY_RANKINGS:
+      return { day: action.payload.data, ...state };
+    case GET_WEEK_RANKINGS:
+      return { week: action.payload.data, ...state };
+    case GET_MONTH_RANKINGS:
+      return { month: action.payload.data, ...state };
     default:
       return state;
   }
