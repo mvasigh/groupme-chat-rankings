e9dc932c1c3c90b8cd35f3c6181d7d64d26898d4
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-07-31 22:38:33 -0500

    wrap app contents in centered column

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import Section from './components/Section';
 import Heading from './components/Heading';
+import Columns from './components/Columns';
+import Column from './components/Column';
 import MessageRankings from './containers/MessageRankings';
 
 class App extends Component {
   render() {
     return (
       <Section>
-        <Heading type="title" size={4}>
-          Good Ole... Rankings
-        </Heading>
-        <Heading type="subtitle" size={6}>
-          See all the ranks
-        </Heading>
-        <MessageRankings />
+        <Columns>
+          <Column size="6 is-offset-3">
+            <Heading type="title" size={4}>
+              Good Ole... Rankings
+            </Heading>
+            <Heading type="subtitle" size={6}>
+              See all the ranks
+            </Heading>
+            <MessageRankings />
+          </Column>
+        </Columns>
       </Section>
     );
   }
