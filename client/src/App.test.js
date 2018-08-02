a544b1d23aa902210b08d6bb7d3ac6816aee01b0
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-01 20:05:36 -0500

    write basic tests for App

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import MessageRankings from './containers/MessageRankings';
+import Heading from './components/Heading';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+it('renders the message rankings', () => {
+  expect(wrapped.find(MessageRankings).length).toEqual(1);
+});
+
+it('renders the title', () => {
+  expect(wrapped.find('[type="title"]').length).toEqual(1);
+});
+
+it('renders the subtitle', () => {
+  expect(wrapped.find('[type="subtitle"]').length).toEqual(1);
 });
