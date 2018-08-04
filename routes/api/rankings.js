42f2318b5fb20d95abf2d398e6ea898cf03fc58a
Mehdi Vasigh <mehdivasigh@gmail.com>
2018-08-04 09:23:45 -0500

    rewrite route handler to accomodate new library

--- routes/api/rankings.js
+++ routes/api/rankings.js
@@ -1,14 +1,20 @@
 const express = require('express'),
   router = express.Router(),
-  GroupChat = require('../../lib/GroupChat');
+  _ = require('lodash'),
+  GroupChat = require('../../lib/GroupChat'),
+  Algo = require('../../lib/Algo');
 
 const GROUP_ID = process.env.GROUP_ID || require('../../config/keys').GROUP_ID;
 const groupChat = new GroupChat(GROUP_ID);
 
 router.get('/', async (req, res) => {
   const period = req.query.period || 'week';
-  const rankings = await groupChat.getRankings(period);
-  res.json(rankings);
+  const leaderboard = await groupChat.getRankings(period);
+  Promise.all(leaderboard).then(messages => {
+    const rankings = messages.map(message => Algo.getScoredMessage(message));
+    const sorted = _.sortBy(rankings, 'score').reverse();
+    res.json(sorted);
+  });
 });
 
 module.exports = router;
